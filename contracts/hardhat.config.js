dc94a3ae638d7ae9d57f7b4733afaf173447e861
SHREE-PRAVEEN <SHREE-kpraveen206@yahoo.com>
2026-01-03 07:01:56 +0530

    blockchain

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,17 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
